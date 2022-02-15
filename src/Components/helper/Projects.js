8bd0cbbfd34bcd4380b2239ff0133ec47115678f
Brayan Leyva <brayanleyvag@gmail.com>
2022-02-15 16:29:57 -0600

    Portfolio hover

--- src/Components/helper/Projects.js
+++ src/Components/helper/Projects.js
@@ -9,31 +9,51 @@ export const Projects = () => {
   return (
     <div className='projects-box'>
         <div className='div-port'>
-          <a href='https://brayanleyvag.github.io/PokeDex/' target='_blank'><img src={proj1} alt='PokeDex'></img></a>
-          <div className='port-btns'>
-            <a href='https://brayanleyvag.github.io/PokeDex/' target='_blank'><button>Demo</button></a>
-            <a href='https://github.com/BrayanLeyvaG/PokeDex' target='_blank'><button>Code</button></a>
+          <img src={proj1} alt='PokeDex'></img>
+          <div className='image-overlay port-btns'>
+            <h5>PokeDex</h5>
+            <p>Search a Pokemon.</p>
+            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-css3-simple"></i> <i className="uil uil-java-script"></i></p>
+            <div className='port-btns'>
+              <a href='https://brayanleyvag.github.io/PokeDex/' target='_blank'><button>Demo</button></a>
+              <a href='https://github.com/BrayanLeyvaG/PokeDex' target='_blank'><button>Code</button></a>
+            </div>
           </div>
         </div>
         <div className='div-port'>
-          <a href='https://brayanleyvag.github.io/gif-app/' target='_blank' target='_blank'><img src={proj2} alt='GifApp'></img></a>
-          <div className='port-btns'>
-            <a href='https://brayanleyvag.github.io/gif-app/' target='_blank'><button>Demo</button></a>
-            <a href='https://github.com/BrayanLeyvaG/gif-app'><button>Code</button></a>
+          <img src={proj2} alt='GifApp'></img>
+          <div className='image-overlay port-btns'>
+            <h5>GifApp</h5>
+            <p>Search and display gifs.</p>
+            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-css3-simple"></i> <i className="uil uil-java-script"></i> <i className="uil uil-react"></i></p>
+            <div className='port-btns'>
+              <a href='https://brayanleyvag.github.io/gif-app/' target='_blank'><button>Demo</button></a>
+              <a href='https://github.com/BrayanLeyvaG/gif-app' target='_blank'><button>Code</button></a>
+            </div>
           </div>
         </div>
         <div className='div-port'>
-          <a href='https://brayanleyvag.github.io/energy-counter/' target='_blank'><img src={proj3} alt='Energy Counter'></img></a>
-          <div className='port-btns'>
-            <a href='https://brayanleyvag.github.io/energy-counter/' target='_blank'><button>Demo</button></a>
-            <a href='https://github.com/BrayanLeyvaG/energy-counter' target='_blank'><button>Code</button></a>
+          <img src={proj3} alt='Email sending simulator'></img>
+          <div className='image-overlay port-btns'>
+            <h5>Email sending</h5>
+            <p>Simulate sending an email.</p>
+            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-java-script"></i> <i className="uil uil-bold"></i></p>
+            <div className='port-btns'>
+              <a href='https://send-email-s.netlify.app/' target='_blank'><button>Demo</button></a>
+              <a href='https://github.com/BrayanLeyvaG/simulating-send-email' target='_blank'><button>Code</button></a>
+            </div>
           </div>
         </div>
         <div className='div-port'>
-          <a href='https://practica-carrito-blg.netlify.app/' target='_blank'><img src={proj4} alt='Shopping Cart project'></img></a>
-          <div className='port-btns'>
-            <a href='https://practica-carrito-blg.netlify.app/' target='_blank'><button>Demo</button></a>
-            <a href='https://github.com/BrayanLeyvaG/practica-carrito' target='_blank'><button>Code</button></a>
+          <img src={proj4} alt='Shopping Cart project'></img>
+          <div className='image-overlay port-btns'>
+            <h5>Shopping Cart</h5>
+            <p>Simulate adding items to cart.</p>
+            <p><i className="uil uil-java-script"></i> <i className="uil uil-react"></i></p>
+            <div className='port-btns'>
+              <a href= 'https://practica-carrito-blg.netlify.app/' target='_blank'><button>Demo</button></a>
+              <a href='https://github.com/BrayanLeyvaG/practica-carrito' target='_blank'><button>Code</button></a>
+            </div>
           </div>
         </div>   
     </div>
