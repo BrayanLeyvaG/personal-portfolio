1733612de85bb0105aba64a2f8ca4f8327df004f
Brayan Leyva <brayanleyvag@gmail.com>
2022-03-22 03:00:46 -0600

    projects added

--- src/Components/helper/Projects.js
+++ src/Components/helper/Projects.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import proj1 from '../../img/proj1.png'
 import proj2 from '../../img/proj2.jpg'
-import proj3 from '../../img/proj3.jpg'
+import proj3 from '../../img/project-to-do.png'
 import proj4 from '../../img/proj4.png'
 import proj5 from '../../img/proj5.jpg'
-import proj6 from '../../img/proj6.jpg'
+import proj6 from '../../img/project-cars.png'
 
 
 export const Projects = () => {
   return (
     <div className='projects-box'>
         <div className='div-port'>
+          <img src={proj3} alt='To-Do list'></img>
+          <div className='image-overlay port-btns'>
+            <h5>Email sending</h5>
+            <p>C.R.U.D. using react-hook-form & http requests.</p>
+            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-css3-simple"></i> <i className="uil uil-react"></i></p>
+            <div className='port-btns'>
+              <a href='https://todoapp-crud.netlify.app/' target='_blank'><button>Demo</button></a>
+              <a href='https://github.com/BrayanLeyvaG/ToDo-CRUD' target='_blank'><button>Code</button></a>
+            </div>
+          </div>
+        </div>
+        <div className='div-port'>
           <img src={proj4} alt='Shopping Cart project'></img>
           <div className='image-overlay port-btns'>
             <h5>DigitalLab</h5>
             <p>Landing page.</p>
-            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-css3-simple"></i> <i className="uil uil-java-script"></i> <i className="uil uil-react"></i></p>
+            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-css3-simple"></i> <i className="uil uil-react"></i></p>
             <div className='port-btns'>
               <a href= 'https://digitallab.netlify.app/' target='_blank'><button>Demo</button></a>
               <a href='https://github.com/BrayanLeyvaG/digitallab' target='_blank'><button>Code</button></a>
@@ -47,6 +59,18 @@ export const Projects = () => {
           </div>
         </div>
         <div className='div-port'>
+          <img src={proj6} alt='Shopping Cart project'></img>
+          <div className='image-overlay port-btns'>
+            <h5>C.R.U.D. Cars</h5>
+            <p>C.R.U.D using controlled inputs & http requests.</p>
+            <p><i className="uil uil-java-script"></i></p>
+            <div className='port-btns'>
+              <a href= 'https://cars-crud-blg.netlify.app/' target='_blank'><button>Demo</button></a>
+              <a href='https://github.com/BrayanLeyvaG/crud-cars' target='_blank'><button>Code</button></a>
+            </div>
+          </div>
+        </div>      
+        <div className='div-port'>
           <img src={proj5} alt='PokeDex'></img>
           <div className='image-overlay port-btns'>
             <h5>Gastelek</h5>
@@ -57,30 +81,7 @@ export const Projects = () => {
             </div>
           </div>
         </div>
-        <div className='div-port'>
-          <img src={proj3} alt='Email sending simulator'></img>
-          <div className='image-overlay port-btns'>
-            <h5>Email sending</h5>
-            <p>Simulate sending an email.</p>
-            <p><i className="uil uil-html5-alt"></i> <i className="uil uil-java-script"></i> <i className="uil uil-bold"></i></p>
-            <div className='port-btns'>
-              <a href='https://send-email-s.netlify.app/' target='_blank'><button>Demo</button></a>
-              <a href='https://github.com/BrayanLeyvaG/simulating-send-email' target='_blank'><button>Code</button></a>
-            </div>
-          </div>
-        </div>
-        <div className='div-port'>
-          <img src={proj6} alt='Shopping Cart project'></img>
-          <div className='image-overlay port-btns'>
-            <h5>Car finder</h5>
-            <p>Search by options.</p>
-            <p><i className="uil uil-java-script"></i></p>
-            <div className='port-btns'>
-              <a href= 'https://buscador-autos-blg.netlify.app/' target='_blank'><button>Demo</button></a>
-              <a href='https://github.com/BrayanLeyvaG/Buscadordeautos' target='_blank'><button>Code</button></a>
-            </div>
-          </div>
-        </div>      
+
     </div>
     );
 };
